c1c2ac37e1c09a806058d2f7b1305d82e54a371e
Jesus Martin <Jesus.martin@clavisinsight.com>
2020-06-01 17:50:17 +0100

    updating rails to solve security issues

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_01_161114) do
+ActiveRecord::Schema.define(version: 2020_06_01_164550) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
